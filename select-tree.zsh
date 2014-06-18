8b6515a5ccdefd24470b171520bec365d6dd6aa3
Takumasa Sakao <sakataku7@gmail.com>
2014-06-18 22:39:17 +0900

    とりあえず完成

---
+++ select-tree.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# treeで色々実行したいときとかあるかもね
+
+# 括弧とかスペースとか置き換えないと気持ちわるい。
+
+if exists percol; then
+	function percol_tree() {
+		selected=$(tree -f | percol --match-method migemo)
+		local new_left="${LBUFFER} ${selected##* }"
+	    BUFFER=${new_left}$RBUFFER
+        CURSOR=${#new_left}
+        zle reset-prompt
+	}
+
+    bindkey '^ti' percol_tree
+fi
